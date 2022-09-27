ae6df161f13ac6bffbdbd8978a0bcde658b3dc8e
kyamranaliyev <111323957+kyamranaliyev@users.noreply.github.com>
2022-09-27 10:46:09 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'admin',
+	    'sonar.login': '42df2d68322772791a4fa9a30242c6a2990b591a',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
